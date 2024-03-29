eacb9fa2ace98a2db3db6530519aa7319dffa442
ByteData96 <thiago.deones@gmail.com>
2024-03-29 12:00:39 -0300

    Condicionais

---
+++ controle-fluxo/src/Detran.java
@@ -0,0 +1,11 @@
+public class Detran {
+    public static void main(String[] args) {
+        int nota = 20;
+
+        if(nota >= 21) {
+            System.out.println("Você foi aprovado!");
+        } else {
+            System.out.println("Você foi reprovado!");
+        }
+    }
+}
